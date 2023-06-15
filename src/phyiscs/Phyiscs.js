d6b2de578e19972368668a75bae648b5b1b8b9b8
William Odeh <williamalodeh@gmail.com>
2023-06-15 20:08:49 +0300

    add class for Vector with methods

--- src/phyiscs/Phyiscs.js
+++ src/phyiscs/Phyiscs.js
@@ -1,11 +1,8 @@
 class Phyiscs {
 
     volicity = () => {
-        // some changes by youmna
-        // some changes by william
-        // william please done change anything
-        this.x = 0
-        this.Z = 0
+
+
     }
 
     acc = () => {
--- src/phyiscs/Vector.js
+++ src/phyiscs/Vector.js
@@ -1,5 +1,36 @@
 class Vector {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
+    add(vector) {
+        return new Vector(this.x + vector.x, this.y + vector.y, this.z + vector.z);
+    }
+
+    subtract(vector) {
+        return new Vector(this.x - vector.x, this.y - vector.y, this.z - vector.z);
+    }
+
+    dot(vector) {
+        return this.x * vector.x + this.y * vector.y + this.z * vector.z;
+    }
+
+    cross(vector) {
+        const x = this.y * vector.z - this.z * vector.y;
+        const y = this.z * vector.x - this.x * vector.z;
+        const z = this.x * vector.y - this.y * vector.x;
+        return new Vector(x, y, z);
+    }
+
+    magnitude() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2);
+    }
+
+    normalize() {
+        const magnitude = this.magnitude();
+        return new Vector(this.x / magnitude, this.y / magnitude, this.z / magnitude);
+    }
 }
-
 export default Vector
