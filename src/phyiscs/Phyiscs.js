001f65fd2c8b7e78970d65bd1ae061eff13b0412
yomnaHilwany <yomnahilwany@gmail.com>
2023-06-16 03:34:43 +0300

    vector update

--- src/phyiscs/Phyiscs.js
+++ src/phyiscs/Phyiscs.js
@@ -5,11 +5,8 @@ class Phyiscs {
         // some changes by william
         // william please done change anything
         this.x = 0
-<<<<<<< HEAD
         this.Z = 0
             // asljdbnajsbdjasbdasjbdjoasbdjoab
-=======
->>>>>>> main
     }
 
     acc = () => {
